7bbbab3b23b1443b2d5afa7429c2778221fe5eda
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2021-08-18 19:52:32 +0100

    added app icon

--- lib/GlobalComponents/GlobalFunctions/SpotifyResultsFunctions.dart
+++ lib/GlobalComponents/GlobalFunctions/SpotifyResultsFunctions.dart
@@ -10,19 +10,20 @@ import 'package:fonz_music_flutter/GlobalComponents/Objects/Track.dart';
 List<Track> tracksToList(tracks) {
   List<Track> trackList = [];
 
-  log("in tracksToList");
-  log("tracks are " + tracks.toString());
+  log("in tracksToList for search");
+  // log("tracks are " + tracks.toString());
     for (var i in tracks) {
       var albumArt = i.album.images[0].url;
-      log("album art is " + i.album.images[0].url);
+      // log("album art is " + i.album.images[0].url);
       var listArtist = i.artists;
-      log("artist is " + i.artists.toString());
-      var externalUrl = i.externalUrls.spotify;
-      log("external is " + i.externalUrls.spotify);
+      // log("artist is " + i.artists.toString());
       List<String> listArtistString = [];
-      listArtist.forEach((e) => {listArtistString.add(e.name)});
+      listArtist.forEach((e) {
+        // log("name is " + e.name);
+        listArtistString.add(e.name);
+      });
       var post = new Track(i.name, listArtistString, i.id, albumArt);
-      log("post is " + post.title.toString());
+      // log("post is " + post.title.toString());
       trackList.add(post);
     }
 
