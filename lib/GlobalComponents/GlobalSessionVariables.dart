038c27efa154a21f5f92d5025a3c9d6323150a82
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2021-09-16 12:24:24 +0100

    updated top artists defaults

--- lib/GlobalComponents/GlobalSessionVariables.dart
+++ lib/GlobalComponents/GlobalSessionVariables.dart
@@ -41,15 +41,15 @@ var tempTracks = [
 List<Artist> topArtists;
 var updateTopArtists = true;
 var tempArtists = [
-  Artist("Red Hot Chili Peppers", "0L8ExT028jH3ddEcZwqJJ5", "https://i.scdn.co/image/89bc3c14aa2b4f250033ffcf5f322b2a553d9331"),
-  Artist( "Kings of Leon", "2qk9voo8llSGYcZ6xrBzKx", "https://i.scdn.co/image/168a281f4a0b1c2c61acb010239ead4710a234a3"),
-  Artist( "Circa Waves", "6hl5k4gLl1p3sjhHcb57t2", "https://i.scdn.co/image/4a8d60073c6ce7007ac43a4807b36f0abd381028"),
+  Artist("Gus Dapperton", "6sHCvZe1PHrOAuYlwTLNH4", "https://i.scdn.co/image/ab6761610000e5ebb9583cd924906a133356770b"),
+  Artist( "Sticky Fingers", "3ZGr7nQBXDU2WhyXgRVbt0", "https://i.scdn.co/image/ab6761610000e5eb1ce40fb5594af15406d233d0"),
+  Artist( "Jafaris", "69hGavzu5tphYU1EIY0yEg", "https://i.scdn.co/image/ab6761610000e5ebccbb4acd25d81442c1ab5bf8"),
   Artist("Hippo Campus", "1btWGBz4Uu1HozTwb2Lm8A",  "https://i.scdn.co/image/ab6761610000e5eb220b2af522d5044b93fc678e"),
-  Artist("COIN", "0ZxZlO7oWCSYMXhehpyMvE", "https://i.scdn.co/image/ab6761610000e5eb8a331b3acc328de052617020"),
-  Artist("alt-J", "3XHO7cRUPCLOr6jwp8vsx5",  "https://i.scdn.co/image/7ac54cbec2f1b3f5f1b7f6fc23acb9d00c70fb51"),
-  Artist("BENEE", "0Cp8WN4V8Tu4QJQwCN5Md4", "https://i.scdn.co/image/cf1265cb1c2c35d253cbbac9b1489bc181322ed3"),
+  Artist( "Txmmy Rose", "1bcfct7lYeMK7cqCDFRGYo", "https://i.scdn.co/image/ab6761610000e5ebb7ef98ac88a29bd18d80b3fd"),
   Artist("girl in red", "3uwAm6vQy7kWPS2bciKWx9", "https://i.scdn.co/image/ebff5a127cf8fbb20deb9bbcd02cfea64a660bef"),
-  Artist("Rush", "2Hkut4rAAyrQxRdof7FVJq", "https://i.scdn.co/image/6fdfa7c623d77d5900e69eef2443340e3493a4bf")
+  Artist( "Mac Miller", "4LLpKhyESsyAXpc4laK94U", "https://i.scdn.co/image/ab6761610000e5ebed3b89aa602145fde71a163a"),
+  Artist( "Dominic Fike", "6USv9qhCn6zfxlBQIYJ9qs", "https://i.scdn.co/image/ab6761610000e5eb436d45fb9b11ad089e0e3c11"),
+  Artist( "Peach Pit", "6fC2AcsQtd9h4BWELbbire", "https://i.scdn.co/image/ab6761610000e5ebb95318e678c40fc2f02c0722"),
 ];
 
 List<Playlist> topPlaylists;
